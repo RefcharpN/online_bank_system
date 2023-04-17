766ee661dba1efa419e3052628ba78bf4d4057d9
smelkin_nikita <nik24sml2000@gmail.com>
2023-04-17 17:29:18 +0300

    мелкие изменения: подготовка к внедрению регистрации

--- app/src/main/java/com/example/test_online_bank_kotlin/Client.kt
+++ app/src/main/java/com/example/test_online_bank_kotlin/Client.kt
@@ -74,7 +74,7 @@ internal class ClientSomthing(private val addr: String, private val port: Int)
     public fun downService() {
         try {
             val jsonObj_out = JSONObject()
-            jsonObj_out!!.put("OPERATION","-1")//TODO: операция номер -1 - разрыв соединения
+            jsonObj_out.put("OPERATION","-1")//TODO: операция номер -1 - разрыв соединения
             this.send(jsonObj_out.toString())
 
             if (!socket!!.isClosed) {
